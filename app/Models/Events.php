3316b6450372aae5b445245599d0eb41bdc07d8a
ashrafAli23 <ashrafammar230@gmail.com>
2022-09-01 12:06:42 +0200

    create model events

---
+++ app/Models/Events.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Events extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'en_title',
+        'ar_title',
+        'en_description',
+        'ar_description',
+        'image',
+        'date'
+    ];
+}
